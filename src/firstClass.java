a3bc01ca9e4317384f8eab456baaaa63b63c3436
Piotr Kowalski <piotrkowalski2003@icloud.com>
2025-10-16 10:39:38 +0200

    firstclass commit

---
+++ src/firstClass.java
@@ -0,0 +1,8 @@
+public class firstClass {
+    public static void main(String[] args) {
+        for (int a = 0; a < 5; a++){
+            System.out.println(a);
+            // for loop
+        }
+    }
+}
