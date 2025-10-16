8fa0179aaadffdb2e5dec5b001093c20500b0eab
Piotr Kowalski <piotrkowalski2003@icloud.com>
2025-10-16 10:42:54 +0200

    main class changes commit

--- src/main.java
+++ src/main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        for(int a = 0; a < 5; a++){
+            System.out.println(a);
+        }
     }
 }
